2a6c9742e7efb1916f2aa31fb640b3a4408962d5
aayushrasali <rasailiaayu197@gmail.com>
2024-07-04 07:27:55 +0545

    Function

---
+++ function.py
@@ -0,0 +1,61 @@
+# def sum(x,y):
+#     Addd = x + y
+#     return Addd
+
+# Addd = sum(2,4)
+# print (Addd)
+
+
+
+# args and **kwargs
+
+# def abc (*args):
+#     sum = 0 
+#     for num in args:
+#         sum = sum + num
+#     return sum 
+    
+# sum = abc(2,3,4,5,6,7,8)
+# print (sum)
+# sum = abc(1,2,3)
+# print (sum)
+
+# ecommers websit
+
+# sub = {
+#     'math':12,
+#     'eng':13,
+# }
+# def percentage (**kwargs):
+#     sum = 0 
+#     for key,value in kwargs.items():
+#         sum = sum + value 
+        
+#     return sum
+
+# sum = percentage(math = 99 , eng=33)
+# print (sum)
+
+
+# function
+# def add(x,y):
+#     return x+y
+
+# print (add(2,4))
+
+# lambda function
+
+# add = lambda x,y: x+y
+# print (add(2,4))
+    
+# 23,30,2
+
+
+
+
+
+
+
+
+
+
